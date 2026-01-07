f10f318fd667856c350e1da8711821f9956743de
FranciscoSalomonMaier <110343152+FranciscoSalomonMaier@users.noreply.github.com>
2026-01-06 22:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ Blog.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Blog.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Blog.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Blog.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
