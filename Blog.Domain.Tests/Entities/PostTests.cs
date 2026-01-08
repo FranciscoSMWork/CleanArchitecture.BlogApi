8a4242873af16d15c21ee8393f67c5ddc8f6bfa0
FranciscoSalomonMaier <110343152+FranciscoSalomonMaier@users.noreply.github.com>
2026-01-08 12:38:28 -0300

    ### Created - Post model and Post test

--- Blog.Domain.Tests/Entities/PostTests.cs
+++ Blog.Domain.Tests/Entities/PostTests.cs
@@ -1,13 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Blog.Domain.Entities;
+using Blog.Domain.Exceptions;
+using Blog.Domain.ValueObjects;
+using FluentAssertions;
 
 namespace Blog.Domain.Tests.Entities;
 
 public class PostTests
 {
- 
+    //Criar post com título e conteúdo válidos
+    [Fact]
+    public async Task CreatePost_WhenDataIsValid()
+    {
+        //Arrange
+        string title = "Post Title Test";
+        string content = "Post Content Test";
+
+        string email = "test@test.com";
+        Email emailCreated = new Email(email);
+
+        string name = "testName";
+        string bio = "bioTest";
+
+        User author = new User(name, emailCreated, bio);
+
+        //Act
+        Post post = new Post(title, content, author);
+
+        //Assert
+        Assert.NotNull(post);
+        Assert.Equal(title, post.Title);
+        Assert.Equal(content, post.Content);
+        Assert.Equal(author, post.Author);
+    }
+
+    //Não permitir post sem autor
+    [Fact]
+    public async Task CreatePost_WhenAuthorIsNotDefined_ShouldReturnError()
+    {
+        //Arrange
+        string title = "Post Title Test";
+        string content = "Post Content Test";
+
+        string email = "test@test.com";
+        Email emailCreated = new Email(email);
+
+        User user = null;
+
+        //Act
+        Action act = () => new Post(title, content, user);
+
+        //Assert
+        act.Should()
+            .Throw<ValueCannotBeEmptyException>("Author")
+            .WithMessage("Author cannot be empty.");
+    }
+
+    //Não permitir título vazio
+    [Fact]
+    public async Task CreatePost_WhenTitleIsEmpty_ShouldReturnError()
+    {
+        //Arrange
+        string title = "";
+        string content = "Post Content Test";
+
+        string email = "test@test.com";
+        Email emailCreated = new Email(email);
+
+        string name = "testName";
+        string bio = "bioTest";
+
+        User author = new User(name, emailCreated, bio);
+
+        //Act
+        Action act = () => new Post(title, content, author);
+
+        //Assert
+        act.Should()
+            .Throw<ValueCannotBeEmptyException>("Title")
+            .WithMessage("Title cannot be empty.");
+    }
+
+    //Atualizar conteúdo do post
+    [Fact]
+    public async Task UpdatePost_WhenContentIsCorrect_ShouldUpdate()
+    {
+        //Arrange
+        string title = "";
+        string content = "Post Content Test";
+
+        string email = "test@test.com";
+        Email emailCreated = new Email(email);
+
+        string name = "testName";
+        string bio = "bioTest";
+
+        User author = new User(name, emailCreated, bio);
+
+        Post post = new Post(title, content, author);
+
+        string newContent = "Updated Post Content Test";
+
+        //Act
+        post.UpdateContent(newContent);
+
+        //Assert
+        post.Content.Should().Be(newContent);
+    }
 }
---
+++ Blog.Domain/Entities/Post.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain.Entities;
+
+public class Post
+{
+    private int maxContentLength = 1000;
+
+    public int Id;
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public User Author { get; set; } 
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public Post(string _title, string _content, User _author)
+    {
+        if (string.IsNullOrWhiteSpace(_title))
+            throw new ValueCannotBeEmptyException("Title");
+
+        if (_author == null)
+            throw new ValueCannotBeEmptyException("Author");
+
+        Title = _title;
+        Content = _content;
+
+        Author = _author;
+    }
+
+    public void UpdateContent(string newContent)
+    {
+        if (newContent.Length > maxContentLength)
+            throw new ExceedCaractersNumberException("Content");
+
+        Content = newContent ?? "";
+    }
+
+}
